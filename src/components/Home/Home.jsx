0af393fcb21e8f060ed2e4ea48a720220190f99c
sakibMERN <sakib.sr57@gmail.com>
2024-11-17 22:13:41 +0600

    add json data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='text-4xl text-green-600'>
             This is home.
         </div>
     );
--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const MainLayout = () => {
     return (
         <div>
-            I am main layout.
+            
             <Outlet></Outlet>
         </div>
     );
